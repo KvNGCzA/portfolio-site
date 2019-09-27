8e6d3b64464b68fc641150a73e271d29674ca853
Akanmu Christopher <akanmuoluwadamilare@yahoo.com>
2019-09-27 21:41:21 +0100

    start second stack page (#4)

--- src/views/StacksPage/index.jsx
+++ src/views/StacksPage/index.jsx
@@ -4,6 +4,11 @@ import './index.css';
 import landingImg from '../../images/stackback6.svg';
 import javaLand from '../../images/java-stack.svg';
 import rightArrow from '../../images/right-arrow.svg';
+import stackLandingLeft from '../../images/stackLandingLeft.png';
+import stackLandingRight from '../../images/stackLandingRight.png';
+import stackLandingRightHalf from '../../images/stackLandingRightHalf.png';
+import stackLandingLeftHalf from '../../images/stackLandingLeftHalf.png';
+import StackNavigator from '../../components/StackNavigator';
 
 class StacksPage extends Component {
 
@@ -12,7 +17,9 @@ class StacksPage extends Component {
       <div className="page-content stack-page">
         <div className="slide">
           <div className="jacket">
+
             <div className="content landing">
+              <img src={stackLandingLeft} alt="" className="stackextra stackextra--left"/>
               <div className="img-box">
                 <div className="action-cont">
                   <p className="cont">fullstack</p>
@@ -25,12 +32,15 @@ class StacksPage extends Component {
                 </div>
                 <img src={landingImg} alt="" className="landing-image"/>
               </div>
-              {/* <div className="background-image back" /> */}
-              {/* <div className="background-image front" /> */}
+              <img src={stackLandingRightHalf} alt="" className="stackextra stackextra--right"/>
             </div>
-            <div className="content two">
 
+            <div className="content two">
+              <img src={stackLandingLeftHalf} alt="" className="stackextra stackextra--left"/>
+              <StackNavigator />
+              <img src={stackLandingRight} alt="" className="stackextra stackextra--right"/>
             </div>
+
           </div>
         </div>
       </div>
