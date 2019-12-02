f9e0b6a48731428b7d8fb836cf97c1100c0c60c0
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-12-02 23:11:37 +0100

    make header respond to scroll

--- src/views/SimplisticPage/index.jsx
+++ src/views/SimplisticPage/index.jsx
@@ -6,16 +6,19 @@ import landingImage from '../../images/landing-image.svg';
 import tools from '../../images/tools.svg';
 import chevronRight from '../../images/chevron-right.svg';
 import actions from '../../images/actions.svg';
+import description from '../../images/description.svg';
 
 
 class SimplisticPage extends Component {
   defaultFormValues = {
     body: '',
     email: '',
-    fullname: ''
+    fullname: '',
+    menuback: ''
   };
 
   state = {
+    currentPage: 0,
     socials: [
       {
         link: 'https://docs.google.com/presentation/d/1dxTE0rnvi7HQz5pkEp526ELH0GBFn_zK0Rq4s_BkgEI/edit?usp=sharing',
@@ -34,29 +37,70 @@ class SimplisticPage extends Component {
     formValues: { ...this.defaultFormValues }
   }
 
+  componentDidMount() {
+    document.addEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll = () => {
+    const { currentPage, menuback } = this.state;
+    const height = window.innerHeight;
+    const top = window.pageYOffset;
+    if (top > 270 && !menuback) {
+      this.setState({ menuback: 'rgba(63, 61, 86, 0.5)' })
+    } else if (top < 270 && menuback) {
+      this.setState({ menuback: '' })
+    }
+
+    if (
+      top > (0.8 * height)
+      && top < ((0.8 * height) * 2)
+      && currentPage !== 1
+    ) {
+      this.selectPage(1);
+    } else if (
+      top > (0.8 * (height * 2))
+      && currentPage !== 2
+    ) {
+      this.selectPage(2);
+    } else if (top < (0.8 * height) && currentPage !== 0) {
+      this.selectPage(0);
+    }
+  }
+
+  selectPage = (currentPage) => {
+    this.setState({ currentPage });
+  }
+
   renderHeader = () => {
+    const { currentPage } = this.state;
     return (
       <header className="simplistic-header">
         <div className="simplistic-header--item simplistic-header--item--one">
           <img src={simplisticLogo} alt="logo"/>
         </div>
         <div className="simplistic-header--item simplistic-header--item--two">
-          <ul>
-            <li className="active">
-              <a href="#here">home</a>
-              <span className="active--underline-parent">
+          <ul style={{
+            backgroundColor: this.state.menuback
+          }}>
+            <li className={currentPage === 0 ? 'active' : ''}>
+              <a href="#homepage" onClick={() => this.selectPage(0)}>home</a>
+              {currentPage === 0 ? <span className="active--underline-parent">
                 <span className="active--underline"></span>
-              </span>
+              </span> : null}
             </li>
-            <li>
-              <a href="#here">my tools</a>
+            <li className={currentPage === 1 ? 'active' : ''}>
+              <a href="#tools" onClick={() => this.selectPage(1)}>my tools</a>
+              {currentPage === 1 ? <span className="active--underline-parent">
+                <span className="active--underline"></span>
+              </span> : null}
             </li>
-            <li>
-              <a href="#here">contact</a>
+            <li className={currentPage === 2 ? 'active' : ''}>
+              <a href="#message" onClick={() => this.selectPage(2)}>contact</a>
+              {currentPage === 2 ? <span className="active--underline-parent">
+                <span className="active--underline"></span>
+              </span> : null}
             </li>
           </ul>
-        </div>
-        <div className="simplistic-header--item simplistic-header--item--three">
         </div>
       </header>
     );
@@ -94,11 +138,11 @@ class SimplisticPage extends Component {
         
         <div className="simplistic-body">
 
-          <div className="introduction">
+          <div id="homepage" className="introduction">
             <img src={landingImage} alt="landing"/>
           </div>
 
-          <div className="simplistic-tools">
+          <div id="tools" className="simplistic-tools">
             <div className="simplistic-tools--parent">
               <img src={tools} alt="tools"/>
               <div className="simplistic-tools--buttons">
@@ -116,7 +160,7 @@ class SimplisticPage extends Component {
             </div>
           </div>
 
-          <div className="simplistic-message">
+          <div id="message" className="simplistic-message">
             <form onSubmit={this.handleSubmit}>
               <div className="simplistic-form">
                 <div className="simplistic-form--header">
@@ -157,7 +201,7 @@ class SimplisticPage extends Component {
           </div>   
         </div>
         <footer>
-          <p>Christopher Akanmu {new Date().getFullYear()}</p>
+          <p>&copy; Christopher Akanmu {new Date().getFullYear()}</p>
         </footer>
       </div>
     )
